a73b1744c2722b83d81823f5a16d8fbc42ebdaf2
Paul Constant <constantpaul@hotmail.fr>
2020-11-28 16:57:13 +0100

    Add Doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
 //! Rust wrapper function which checks that the given closure does not modify input data.
+//!
 //! This is helpful when checking that a function which returns Err(...) does not have
 //! side-effects.
+//!
+//! # Example
+//!
+//! ```
+//! use assert_not_modified::assert_not_modified;
+//!
+//! // This bugged function wil return Err but still modify the data.
+//! fn misleading_err(x: &mut i32) -> Result<(), String> {
+//!     *x = *x + 1;
+//!     // Throws an error but x is modified anyway. This is misleading.
+//!     Err("Something wrong happened !".to_owned())
+//! }
+//!
+//! // This test will expose the lying function :
+//! assert!(std::panic::catch_unwind(|| {
+//!     let mut x = 3;
+//!     assert_not_modified(&mut x, |x| misleading_err(x)); // Panics
+//! })
+//! .is_err());
+//! ```
 
 /// Helper function which panics if the given closure mutates the input data.
 ///
